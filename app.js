c2368b025cd287ffaa599897c2dfacf3f0942951
Pressto <adminapi@presstoperu.com>
2020-12-26 15:43:59 -0500

    cors bodyParse

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
 const mercadopago = require("mercadopago");
@@ -52,6 +53,17 @@ let preference = {
     external_reference: "riossuarezcarlos@gmail.com",
     notification_url: ""
 }
+
+// Habilitar CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+    res.header('Access-Control-Allow-Methos', 'GET, POST');
+    next();
+})
+
+// Definir BOdy Parser
+app.use(bodyParser.json());
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -108,6 +120,7 @@ app.get("/failure", function(req,res){
 app.post("/notificaciones", function(req, res){
     // todo lo que manda el mercado pago lo recibo mediante el req.query y esto lo definimos en la linea 87
     console.log(req.query);
+    console.log(req.body);
     res.status(200).send("ok")
 });
 
